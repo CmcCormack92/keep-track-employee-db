107c2294e36aaf0b3ff679ca56c84cdf4b822291
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-15 10:34:37 -0500

    add inquirer prompts

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 const cTable = require('console.table');
+const {mainPrompt, newDepartment, newPosition, newEmployee, updateEmployee} = require('./src/prompts');
+const {viewDept, viewPositions, viewEmployees, addDept, addPos, addEmployee, updateEmp} = require('./utils/index');
+const { update } = require('lodash');
+
+inquirer.prompt(mainPrompt)
+.then(answer => {
+    if (answer === 'View all departments'){
+        viewDept();
+    } 
+    else if (answer === 'View all positions') {
+        viewPositions();
+    }
+    else if (answer === 'View all employees') {
+        viewEmployees();
+    }
+    else if (answer === 'Add a department') {
+        addDept();
+    }
+    else if (answer === 'Add a position') {
+        addPos();
+    }
+    else if (answer === 'Add an employee') {
+        addEmployee();
+    } else {
+        updateEmp();
+    }
+})
--- src/prompts.js
+++ src/prompts.js
@@ -1,13 +1,142 @@
 const inquirer = require('inquirer');
 
-function mainPrompt() {
-    inquirer.prompt([
+const mainPrompt = [
         {
             type: 'list',
             name: 'options',
             message: 'Please choose one of the following options:',
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+            choices: ['View all departments', 'View all positions', 'View all employees', 'Add a department', 'Add a position', 'Add an employee', 'Update an employee role']
         }
-    ])
-};
+    ];
 
+
+const newDepartment = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter new department name!',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the department name!");
+                    return false;
+                }
+            }
+        }
+    ];
+
+const newPosition = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter new position title!',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the position title!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'Enter the yearly salary for this position!',
+            validate: salaryInput => {
+                if (salaryInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the position's salary!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Enter the department this position is in!',
+            validate: departmentInput => {
+                if (departmentInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the department!");
+                    return false;
+                }
+            }
+        }
+    ];
+
+const newEmployee = [
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'Enter employee first name!',
+            validate: firstNameInput => {
+                if (firstNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter employee first name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Enter the employee last name!',
+            validate: lastNameInput => {
+                if (lastNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee last name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'position',
+            message: 'Enter the employee position!',
+            validate: positionInput => {
+                if (positionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee position!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'Enter the manager of this employee',
+            validate: managerInput => {
+                if (managerInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the employee manager!");
+                    return false;
+                }
+            }
+        }
+    ];
+
+const updateEmployee = [
+        {
+            type: 'input',
+            name: 'newPosition',
+            message: 'Enter new employee position!',
+            validate: newPositionInput => {
+                if (newPositionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the new employee position!");
+                    return false;
+                }
+            }
+        }
+    ];
+
+module.exports = {mainPrompt, newDepartment, newPosition, newEmployee, updateEmployee};
